901f9ba067f3fb6b70b8f3786ddb9ddc05470d44
Fox <mccloud.fox21@gmail.com>
2023-09-16 13:10:43 +0200

    Menus are CONTAINERS in 1.18

--- src/main/java/growthcraft/milk/init/GrowthcraftMilkMenus.java
+++ src/main/java/growthcraft/milk/init/GrowthcraftMilkMenus.java
@@ -15,7 +15,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class GrowthcraftMilkMenus {
 
     public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(
-            ForgeRegistries.MENU_TYPES, Reference.MODID
+            ForgeRegistries.CONTAINERS, Reference.MODID
     );
 
     public static final RegistryObject<MenuType<ChurnMenu>> CHURN_MENU =
