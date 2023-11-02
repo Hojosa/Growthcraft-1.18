2a90b2d1ebdbaf35576ac8d7f5a7277b6109d782
Fox <mccloud.fox21@gmail.com>
2023-11-02 22:42:12 +0100

    working loot modifiers

--- src/main/java/growthcraft/core/loot/GlobalLootModifier.java
+++ src/main/java/growthcraft/core/loot/GlobalLootModifier.java
@@ -1,26 +1,23 @@
 package growthcraft.core.loot;
 
-import com.google.common.base.Suppliers;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import com.google.gson.JsonObject;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
-import java.util.function.Supplier;
 
 public class GlobalLootModifier extends LootModifier {
 
-    public static final Supplier<Codec<GlobalLootModifier>> LOOT_MODIFIER = Suppliers.memoize(()
-            -> RecordCodecBuilder.create(inst -> codecStart(inst).and(ForgeRegistries.ITEMS.getCodec()
-            .fieldOf("item").forGetter(m -> m.item)).apply(inst, GlobalLootModifier::new)));
     private final Item item;
 
     protected GlobalLootModifier(LootItemCondition[] conditionsIn, Item item) {
@@ -28,14 +25,25 @@ public class GlobalLootModifier extends LootModifier {
         this.item = item;
     }
 
+    @Nonnull
     @Override
-    protected @NotNull List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
-        generatedLoot.add(new ItemStack(item));
+    protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+        generatedLoot.add(new ItemStack(item, 1));
         return generatedLoot;
     }
 
-    @Override
-    public Codec<? extends IGlobalLootModifier> codec() {
-        return LOOT_MODIFIER.get();
+    public static class Serializer extends GlobalLootModifierSerializer<GlobalLootModifier> {
+        @Override
+        public GlobalLootModifier read(ResourceLocation location, JsonObject object, LootItemCondition[] lootConditions) {
+            Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation((GsonHelper.getAsString(object, "item"))));
+            return new GlobalLootModifier(lootConditions, item);
+        }
+
+        @Override
+        public JsonObject write(GlobalLootModifier instance) {
+            JsonObject json = makeConditions(instance.conditions);
+            json.addProperty("item", ForgeRegistries.ITEMS.getKey(instance.item).toString());
+            return json;
+        }
     }
 }
