386debc63a6421a1002d6d76cd3190e02a3dd017
Fox <mccloud.fox21@gmail.com>
2023-12-28 18:37:53 +0100

    GC Rice datagen

--- src/main/java/growthcraft/rice/datagen/GrowthcraftRiceDataGenerators.java
+++ src/main/java/growthcraft/rice/datagen/GrowthcraftRiceDataGenerators.java
@@ -1,12 +1,9 @@
 package growthcraft.rice.datagen;
 
-import growthcraft.rice.datagen.providers.GrowthcraftRiceBlockTags;
-import growthcraft.rice.datagen.providers.GrowthcraftRiceItemTags;
 import growthcraft.rice.datagen.providers.GrowthcraftRiceLootTables;
 import growthcraft.rice.datagen.providers.GrowthcraftRiceRecipes;
 import growthcraft.rice.shared.Reference;
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
@@ -17,12 +14,8 @@ public class GrowthcraftRiceDataGenerators {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent event) {
 		DataGenerator generator = event.getGenerator();
-		ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 		
 		if(event.includeServer()) {
-			GrowthcraftRiceBlockTags blockTags = new GrowthcraftRiceBlockTags(generator, existingFileHelper);
-			generator.addProvider(blockTags);
-			generator.addProvider(new GrowthcraftRiceItemTags(generator, blockTags, existingFileHelper));
 			generator.addProvider(new GrowthcraftRiceRecipes(generator));
 			generator.addProvider(new GrowthcraftRiceLootTables(generator));
 		}
--- src/main/java/growthcraft/rice/datagen/providers/GrowthcraftRiceRecipes.java
+++ src/main/java/growthcraft/rice/datagen/providers/GrowthcraftRiceRecipes.java
@@ -2,9 +2,18 @@ package growthcraft.rice.datagen.providers;
 
 import java.util.function.Consumer;
 
+import growthcraft.rice.init.GrowthcraftRiceItems;
+import growthcraft.rice.shared.Reference;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.SingleItemRecipeBuilder;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.Tags;
 
 public class GrowthcraftRiceRecipes extends RecipeProvider{
 
@@ -14,6 +23,44 @@ public class GrowthcraftRiceRecipes extends RecipeProvider{
 
 	@Override
 	protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+		ShapedRecipeBuilder.shaped(GrowthcraftRiceItems.CULTIVATOR.get())
+		.pattern("iIi")
+		.pattern("iSi")
+		.pattern(" S ")
+		.define('i', Tags.Items.NUGGETS_IRON)
+		.define('I', Tags.Items.INGOTS_IRON)
+		.define('S', Tags.Items.RODS_WOODEN)
+		.group(Reference.MODID)
+		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.IRON_INGOT))
+		.save(consumer);
+		
+		ShapedRecipeBuilder.shaped(GrowthcraftRiceItems.KNIFE.get())
+		.pattern("  I")
+		.pattern(" I ")
+		.pattern("S  ")
+		.define('I', Tags.Items.INGOTS_IRON)
+		.define('S', Tags.Items.RODS_WOODEN)
+		.group(Reference.MODID)
+		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.IRON_INGOT))
+		.save(consumer);
+		
+		SingleItemRecipeBuilder.stonecutting(Ingredient.of(GrowthcraftRiceItems.RICE_STALK.get()), GrowthcraftRiceItems.RICE.get())
+		.group(Reference.MODID)
+		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(GrowthcraftRiceItems.RICE_STALK.get()))
+		.save(consumer);
+		
+		ShapedRecipeBuilder.shaped(GrowthcraftRiceItems.SUSHI_ROLL.get())
+		.pattern("F")
+		.pattern("R")
+		.pattern("K")
+		.define('F', ItemTags.FISHES)
+		.define('R', GrowthcraftRiceItems.RICE_COOKED.get())
+		.define('K', Items.KELP)
+		.group(Reference.MODID)
+		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(GrowthcraftRiceItems.RICE_COOKED.get()))
+		.save(consumer);
+		
+		
 		
 	}
 	
