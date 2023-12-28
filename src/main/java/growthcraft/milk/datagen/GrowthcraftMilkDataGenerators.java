e627cd008c5d34461d4dedfd3bc0589112dc039c
Fox <mccloud.fox21@gmail.com>
2023-12-28 18:36:47 +0100

    GC Milk datagen

--- src/main/java/growthcraft/milk/datagen/GrowthcraftMilkDataGenerators.java
+++ src/main/java/growthcraft/milk/datagen/GrowthcraftMilkDataGenerators.java
@@ -1,12 +1,9 @@
 package growthcraft.milk.datagen;
 
-import growthcraft.milk.datagen.providers.GrowthcraftMilkBlockTags;
-import growthcraft.milk.datagen.providers.GrowthcraftMilkItemTags;
 import growthcraft.milk.datagen.providers.GrowthcraftMilkLootTables;
 import growthcraft.milk.datagen.providers.GrowthcraftMilkRecipes;
 import growthcraft.milk.shared.Reference;
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
@@ -17,12 +14,8 @@ public class GrowthcraftMilkDataGenerators {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent event) {
 		DataGenerator generator = event.getGenerator();
-		ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 		
 		if(event.includeServer()) {
-			GrowthcraftMilkBlockTags blockTags = new GrowthcraftMilkBlockTags(generator, existingFileHelper);
-			generator.addProvider(blockTags);
-			generator.addProvider(new GrowthcraftMilkItemTags(generator, blockTags, existingFileHelper));
 			generator.addProvider(new GrowthcraftMilkRecipes(generator));
 			generator.addProvider(new GrowthcraftMilkLootTables(generator));
 		}
